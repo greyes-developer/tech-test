de1fc7aa2de60db8314d68150d1047f876027147
Gustavo Reyes Martinez <gustavo.reyes@dedica.com.mx>
2021-06-23 13:18:34 -0500

    Text test bootstrap created

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hola mundo</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
